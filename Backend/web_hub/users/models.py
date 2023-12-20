3a63f11d182f8f690ccdbe2cf4b0c25fff3f114b
Bonaventure Anselm <bona.anselm@gmail.com>
2023-12-20 13:36:55 +0100

    Implemented models and Inline display admin configs

--- Backend/web_hub/users/models.py
+++ Backend/web_hub/users/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1,
+                              choices=GENDER_CHOICES,
+                              blank=True,
+                              default='M',
+                              help_text='Select Your Gender'
+                            )
+    date_of_birth = models.DateField(null=True, blank=True)
+    phone_number = models.CharField(max_length=15)
+    skills = models.ManyToManyField('Skills')
+    job_title = models.ManyToManyField('JobTitle')
+    specialization = models.ManyToManyField('Specialization')
+    location = models.CharField(max_length=100)
+    social_media_handles = models.CharField(max_length=100, default='X')
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+    class Meta:
+        ordering = ['user__first_name', 'user__last_name']
+    
+    def __str__(self):
+        """ String for representing the Model object. """
+        return f'{self.user.first_name} Profile'
+
+    def get_absolute_url(self):
+        """ Returns the URL to access a user profile """
+        return reverse('user-profile', args=[str(self.id)])
+
+
+class Skills(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        """ String for representing the Model object. """
+        return self.name
+
+    def get_absolute_url(self):
+        """ Returns the URL to access a particular skill """
+        return reverse('skill-detail', args=[str(self.id)])
+
+
+class Specialization(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        """ String for representing the Model object. """
+        return self.name
+
+    def get_absolute_url(self):
+        """ Returns the URL to access a particular specialization """
+        return reverse('specialization-detail', args=[str(self.id)])
+
+
+class JobTitle(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        """ String for representing the Model object. """
+        return self.title
+
+    def get_absolute_url(self):
+        """ Returns the URL to access a particular job title """
+        return reverse('job-title', args=[str(self.id)])
--- Backend/web_hub/web_hub/settings.py
+++ Backend/web_hub/web_hub/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'project_creation.apps.ProjectCreationConfig',
+    'emoji_picker',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
