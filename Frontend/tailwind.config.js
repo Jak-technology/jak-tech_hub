06fd17305e7dfe93eceb5cc9b9f83b2fe3d68d94
Jak-technology <ajosh22.aj@gmail.com>
2024-01-15 10:05:55 -0800

    okay

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./build/*.js"],
+  content: ["./build/**/*.html", "./build/**/*.js"],
   theme: {
     extend: {
       screens: {
