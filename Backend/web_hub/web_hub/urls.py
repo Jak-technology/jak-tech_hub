69c31b742938b5c9c3e7ef7a7dcfc6cf9bb28ba4
Bonaventure Anselm <bona.anselm@gmail.com>
2024-02-27 13:59:17 +0100

    migrations

--- Backend/web_hub/web_hub/urls.py
+++ Backend/web_hub/web_hub/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
+    path('', include('services.urls')),
 ]
 
 
