3eb0f9b9c38f1e75ec50921f8349df5a7d502f42
Bonaventure Anselm <bona.anselm@gmail.com>
2024-03-01 21:56:12 +0100

    Created service-request.js file

---
+++ Frontend/Js/service-request.js
@@ -0,0 +1,72 @@
+async function populateSelectOptions() {
+    try {
+        const response = await fetch('http://localhost:8000/api/choices/');
+        const data = await response.json();
+        const { contact_choices, service_choices, business_type_choices } = data;
+
+        // Populate Preferred Method of Contact
+        const preferredContactSelect = document.getElementById('preferredContact');
+        for (const [key, value] of Object.entries(contact_choices)) {
+            const option = document.createElement('option');
+            option.value = key;
+            option.textContent = value;
+            preferredContactSelect.appendChild(option);
+        }
+
+        // Populate Services Needed
+        const servicesNeededSelect = document.getElementById('servicesNeeded');
+        for (const [key, value] of Object.entries(service_choices)) {
+            const option = document.createElement('option');
+            option.value = key;
+            option.textContent = value;
+            servicesNeededSelect.appendChild(option);
+        }
+
+        // Populate Business Type
+        const businessTypeSelect = document.getElementById('businessType');
+        for (const [key, value] of Object.entries(business_type_choices)) {
+            const option = document.createElement('option');
+            option.value = key;
+            option.textContent = value;
+            businessTypeSelect.appendChild(option);
+        }
+    } catch (error) {
+        console.error('Error fetching select options:', error);
+    }
+}
+
+// Function to handle form submission
+// Function to handle form submission
+function handleSubmit(event) {
+event.preventDefault(); // Prevent default form submission
+
+// Get form data
+const formData = new FormData(event.target);
+
+// Submit form data to the specified URL
+fetch('http://127.0.0.1:8000/api/services/', {
+    method: 'POST',
+    body: formData,
+})
+.then(response => {
+    if (response.ok) {
+        console.log('Form submitted successfully');
+        // Redirect to Services page
+        event.target.reset()
+        // Display success message
+        alert('Request Submitted Successfully');
+    } else {
+        console.error('Form submission failed');
+        alert('Request Submission Failed');
+        }
+    })
+    .catch(error => {
+        console.error('Error submitting form:', error);
+        alert('Error Submitting Request');
+    });
+}
+// Call the function to populate select options when the page loads
+window.onload = populateSelectOptions;
+
+// Add event listener for form submission
+document.querySelector('.first-form').addEventListener('submit', handleSubmit);
