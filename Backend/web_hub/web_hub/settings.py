8fa48369af7f53a358bd1a5198c5fe49ac046bff
Iseg6ix <freddyjseg@gmail.com>
2024-03-07 11:25:20 -0500

    build.sh, whitenoise

--- Backend/web_hub/web_hub/settings.py
+++ Backend/web_hub/web_hub/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,7 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR/'assets'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media upload settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
