016af54fc235067c13fb25993929996cb38ee6d8
Iseg6ix <freddyjseg@gmail.com>
2024-03-08 09:23:41 -0500

    Media files settings

--- Backend/web_hub/web_hub/settings.py
+++ Backend/web_hub/web_hub/settings.py
@@ -154,12 +154,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR/'assets'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media upload settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIAFILES_DIRS =[os.path.join(BASE_DIR, 'media')]
+
 MEDIA_URL = '/media/'
 
 
