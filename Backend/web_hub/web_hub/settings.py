bc33be904285b3fcceb18df3c464d78ca80b226c
Iseg6ix <freddyjseg@gmail.com>
2024-03-12 04:07:21 -0400

    New changes for test over http

--- Backend/web_hub/web_hub/settings.py
+++ Backend/web_hub/web_hub/settings.py
@@ -22,15 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3@o-r0*g+!@8jp#x_1cvryo*=ecu^)b4r(!+mo_l@afw-yijo='
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-3@o-r0*g+!@8jp#x_1cvryo*=ecu^)b4r(!+mo_l@afw-yijo='
+# SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = True
+# DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
@@ -114,9 +114,9 @@ DATABASES = {
         'NAME': BASE_DIR / "db.sqlite3",
     }
 }
-database_url = os.environ.get("DATABASE_URL")
-DATABASES["default"] = dj_database_url.parse(database_url)
-# DATABASES['default'] = dj_database_url.parse("postgres://jak_technologies_backend_render_user:UF1iojR9uvwnKVX7Nv206RKWartjtnBZ@dpg-cnks2h2cn0vc73d8jg90-a.oregon-postgres.render.com/jak_technologies_backend_render")
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES["default"] = dj_database_url.parse(database_url)
+DATABASES['default'] = dj_database_url.parse("postgres://jak_technologies_backend_render_user:UF1iojR9uvwnKVX7Nv206RKWartjtnBZ@dpg-cnks2h2cn0vc73d8jg90-a.oregon-postgres.render.com/jak_technologies_backend_render")
 
 
 # Password validation
