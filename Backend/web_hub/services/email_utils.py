59b01751c7e25fa6a3fcafba24a6fc5d0aec926c
Iseg6ix <freddyjseg@gmail.com>
2024-03-26 16:00:58 -0400

    Auto emails-Have a project form ( pending html template)

---
+++ Backend/web_hub/services/email_utils.py
@@ -0,0 +1,21 @@
+import os
+import smtplib
+from django.core.mail import EmailMessage
+from django.conf import settings
+from rest_framework.response import Response
+from email.message import EmailMessage
+
+
+def send_email(subject:str, body:str, recipients:list):
+    try:
+        for person in recipients:
+            msg = EmailMessage()
+            msg['Subject'] = subject
+            msg['From'] = settings.EMAIL_HOST_USER
+            msg['To'] = person
+            msg.set_content(body)
+            with smtplib.SMTP_SSL(settings.EMAIL_HOST, setting.EMAIL_PORT) as smtp:
+                smtp.login(settings.EMAIL_HOST_USER, 'sabc spdo dkpk jqux')
+                smtp.send_message(msg)
+    except Exception as e:
+        print({"Error sending email: ": e})
